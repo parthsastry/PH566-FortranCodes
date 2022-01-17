6bd1ae316cb67d93b06d369203ff2338d59d7401
Parth Sastry <parth.sastry@gmail.com>
2022-01-17 18:37:52 +0530

    minor changes

--- Assignment1/Q1/evaluate2.f90
+++ Assignment1/Q1/evaluate2.f90
@@ -1,10 +1,10 @@
 PROGRAM evaluate2
     REAL*8, parameter :: PI = 4.D0*DATAN(1.D0)
-    REAL :: x = 1.0
-    REAL :: y = 2.0
-    REAL :: a = 15.0
-    REAL :: a_rad
-    REAL :: eval2
+    REAL*8 :: x = 1.0
+    REAL*8 :: y = 2.0
+    REAL*8 :: a = 15.0
+    REAL*8 :: a_rad
+    REAL*8 :: eval2
     a_rad = a*PI/180.0
     eval2 = log10(x) + cos(a_rad) + ABS(x**2 + y**2) + 2*SQRT(x*y)
     write(*,*) eval2
